e61bde792bf10c77d57bd96524017d0832a3cca9
webarata3 <webarata3@gmail.com>
2022-09-21 15:37:43 +0900

    不要なimportの除去

--- elm/AttrType.elm
+++ elm/AttrType.elm
@@ -5,7 +5,6 @@ import Dict exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Json.Decode as JD exposing (Decoder, field, int, string)
 
 
 
